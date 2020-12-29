1335f5b34a596710ee284cd892792bd42dc66813
meghanaraokl <meghanaraokl@gmail.com>
2020-12-29 17:45:46 +0530

    committ

---
+++ UniversityLibrary_Application/src/com/meghana/Book.java
@@ -0,0 +1,51 @@
+package com.meghana;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Book {
+
+	String title;
+	String author;
+	double price;
+	Date d= new Date(0, 0, 0) published_date;
+	Date(int purchased_date;
+	double quantity;
+	  
+//public Book(){
+//this.title="Jonny";
+//this.author="Chintu";
+//this.price=700;
+//this.published_date= Date d= new Date(20, 3, 12);
+//this.purchased_date=Date();
+//this.quantity=7;
+//}
+
+public void AddBook() {
+	 String connectionUrl 
+      "jdbc:sqlserver://LAPTOP-CIKUG36M//SQLEXPRESS;database=University_DB;integratedSecurity=true";
+      String SPsql = "EXEC spInsertBook ?,?,?,?,?,?";   
+       try (Connection connection = DriverManager.getConnection(connectionUrl);) {
+         System.out.println("Connected");
+         
+        PreparedStatement ps = connection.prepareStatement(SPsql);
+         ps.setEscapeProcessing(true);
+         ps.setQueryTimeout(60);
+         
+         ps.setString(1,title);
+         ps.setString(2,author);
+         ps.setDouble(3,70);
+         ps.setDate(4, x);
+         ps.setDate(5, x);
+         ps.setDouble(6,7);
+        
+          
+         ResultSet rs = ps.executeQuery();   }
+          // Handle any errors that may have occurred.
+       catch (SQLException e) {
+       	
+	          }
--- UniversityLibrary_Application/src/com/meghana/Proagram.java
+++ UniversityLibrary_Application/src/com/meghana/Proagram.java
@@ -1,9 +1,75 @@
 package com.meghana;
 
+import java.util.Scanner;
+
 public class Proagram {
-	public static void main(String args[])
-	{
-		barroweer b = new barroweer();
+	public static void main(String args[]) {
+		RegisterBarrower();
+	}
+
+	public static void RegisterBarrower() {
+
+		System.out.println("Enter a title");
+		Scanner s = new Scanner(System.in);
+		String Title = s.nextLine();
+
+		System.out.println("Enter a First_Name");
+		Scanner s2 = new Scanner(System.in);
+		String First_Name = s.nextLine();
+
+		System.out.println("Enter a Last_Name");
+		Scanner s3 = new Scanner(System.in);
+		String Last_Name = s.nextLine();
+
+		System.out.println("Enter a AddressLine1");
+		Scanner s4 = new Scanner(System.in);
+		String AddressLine1 = s.nextLine();
+
+		System.out.println("Enter a AddressLine2");
+		Scanner s5 = new Scanner(System.in);
+		String AddressLine2 = s.nextLine();
+
+		System.out.println("Enter a city");
+		Scanner s6 = new Scanner(System.in);
+		String city = s.nextLine();
+
+		System.out.println("Enter a StateName");
+		Scanner s7 = new Scanner(System.in);
+		String StateName = s.nextLine();
+
+		System.out.println("Enter a Country");
+		Scanner s8 = new Scanner(System.in);
+		String Country = s.nextLine();
+
+		System.out.println("Enter a postCode");
+		Scanner s9 = new Scanner(System.in);
+		int PostCode = s.nextInt();
+
+		System.out.println("Enter a MobileNumberCountryCode");
+		Scanner s10 = new Scanner(System.in);
+		int CountryCode = s.nextInt();
+
+		System.out.println("Enter a MobileNumber");
+		Scanner s11 = new Scanner(System.in);
+		long Mobile_Number = s.nextLong();
+
+		System.out.println("Enter a MobileNumberCountryCode");
+		Scanner s12 = new Scanner(System.in);
+		int Mobile_Number_CountryCode = s.nextInt();
+
+		System.out.println("Enter a Email_id");
+		Scanner s13 = new Scanner(System.in);
+		String Email_id = s.nextLine();
+
+		System.out.println("Enter a barrowertype");
+		Scanner s14 = new Scanner(System.in);
+		String Barrower_type =  s.nextLine();  
+
+		Barrower b = new Barrower(Title, First_Name, Last_Name, AddressLine1, AddressLine2, city, StateName, Country,
+				PostCode, Mobile_Number_CountryCode, Mobile_Number, Email_id, Barrower_type);
 		b.Register();
-		}
-        }   
+	}
+
+	// Barrower b2 = new Barrower(Title,First_Name, Last_Name, AddressLine1,
+	// AddressLine2,city,StateName,Country,PostCode,CountryCode,MobileNUmber,EmailId,BarrowerType);
+}
